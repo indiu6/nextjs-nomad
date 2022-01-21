c4fc509fa84006a5b3079d3e113ed92ed6e40093
indiu6 <hewas6@gmail.com>
2022-01-20 23:04:28 -0500

    add themoviedb api, nexstjs/rewrites

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
+const API_KEY = process.env.API_KEY;
+
 module.exports = {
   reactStrictMode: true,
   // experimental: {
   //   concurrentFeatures: true,
   //   serverComponents: true,
   // },
+  async redirects() {
+    return [
+      {
+        source: '/contact',
+        destination: '/form',
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
 };
