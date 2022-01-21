7c5b2b5a78322eef4122c0ee6c9217443dc50335
indiu6 <hewas6@gmail.com>
2022-01-21 09:07:32 -0500

    add link of movie detail

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import Seo from '../components/Seo';
 
 function Home({ results }) {
@@ -11,16 +13,28 @@ function Home({ results }) {
   //   })();
   // }, []);
 
+  const router = useRouter();
+  const onClick = (id) => {
+    router.push(`/movies/${id}`);
+  };
+
   return (
     <div className="container">
       <Seo title="Home" />
+
       {/* {!movies && <h4>Loading...</h4>} */}
+
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
+        <div onClick={() => onClick(movie.id)} className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <h4>
+            <Link href={`/movies/${movie.id}`}>
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
         </div>
       ))}
+
       <style jsx>{`
         .container {
           display: grid;
@@ -56,6 +70,7 @@ export async function getServerSideProps() {
   const { results } = await (
     await fetch(`http://localhost:3000//api/movies`)
   ).json();
+
   return {
     props: {
       results,
